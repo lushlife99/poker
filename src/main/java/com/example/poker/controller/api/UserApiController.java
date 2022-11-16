d9f5179d48dfbcca76141f8d1b95b23f24b66497
Hodu-moon <dudgh614@gmail.com>
2022-11-16 20:04:51 +0900

    fix

--- src/main/java/com/example/poker/controller/api/UserApiController.java
+++ src/main/java/com/example/poker/controller/api/UserApiController.java
@@ -27,7 +27,7 @@ public class UserApiController {
         // 실제로 DB에 insert를 하고 아래에서 리턴해라아아아아.
         user.setRole(RoleType.User); // 관리자는 필요 없는걸로
         userService.save(user);
-        return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
+        return new ResponseDto<Integer>(1);
         //여기서 Jackson 이라는 라이브러리가 객체를 리턴할 때 json 으로 바꿔서 전달해준다.
     }
 
@@ -40,7 +40,7 @@ public class UserApiController {
             session.setAttribute("principal", principal);
         }
 
-        return  new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
+        return  new ResponseDto<Integer>(1);
 
     }
 }
